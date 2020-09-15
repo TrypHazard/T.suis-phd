d84f8f49704fb59ccdc5a00aa1b6d9ace6f8501a
TrypHazard <67004993+TrypHazard@users.noreply.github.com>
2020-09-15 14:47:35 +0100

    Update linearise_fasta.sh

--- Misc-helpful-scripts/linearise_fasta.sh
+++ Misc-helpful-scripts/linearise_fasta.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # remove new line command for every non-header line in fasta file $1 
-# output all linearised sequence lines under their respective header lines to file **lin_$1.fa**
+# output all linearised sequence lines under their respective header lines to file lin_$1.fa
 
 sed -e 's/\(^>.*$\)/#\1#/' $1 | tr -d “\r” | tr -d “\n” | sed -e ‘s/$/#/#’ | tr “#” “\n” |sed -e '/^$/d' > lin_$1.fa 
 done
