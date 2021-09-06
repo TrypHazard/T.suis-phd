6bc13ea69a7db0cc9bc82adf24787dbd1f4cd9ff
Rachel Hutchinson <67004993+TrypHazard@users.noreply.github.com>
2021-09-06 10:02:40 +0100

    Delete Ch5_genome/Assembly directory

--- Ch5_genome/Assembly/quick_nucmerer.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-reference=$1
-query=$2
-promer --maxgap=500 --mincluster=100 --prefix=NUCMER $1 $2
-show-coords -r NUCMER.delta > NUCMER.coords
-show-aligns NUCMER.delta > NUCMER.aligns
-delta-filter -q -r NUCMER.delta > NUCMER.filter
-mummerplot --png --large NUCMER.filter -R $1 -Q $2 --prefix=NUCMER
